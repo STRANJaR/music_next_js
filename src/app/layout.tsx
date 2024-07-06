44e95191bf459f5125108f2cf313d7e1ea3a2e56
Rohit Shrivastav <rohitrks805@gmail.com>
2024-07-06 13:53:08 +0530

    card component added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
 
       <div className="relative w-full flex items-center justify-center">
         <Navbar/>
       </div>
+      </body>
     </html>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar({ className }: { className?: string }) {
 
   return (
     <div
-    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    className={cn("fixed dark:bg-transparent top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
         <Menu setActive={setActive}>
             <Link href={"/"}>
